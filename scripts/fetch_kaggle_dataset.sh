98604fe9329b7323b0195f4345b305a1df9c741a
Daniel Browne <daniel.browne@datactics.com>
2020-10-26 15:09:45 +0000

    updated kaggle cli script again

--- scripts/fetch_kaggle_dataset.sh
+++ scripts/fetch_kaggle_dataset.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-kaggle competitions download -c house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/datasets/ --unzip
+kaggle competitions download -f train.csv house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/
+kaggle competitions download -f test.csv house-prices-advanced-regression-techniques -p packages/regression_model/regression_model/
